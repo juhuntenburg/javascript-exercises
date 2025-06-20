731e3644c854b785533afaad1d70ac09e4a3b32e
juhuntenburg <ju.huntenburg@gmail.com>
2025-06-20 09:59:09 +0200

    Solve exercises 06 to 11

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let idx;
+    for (let i = 0; i < args.length; i++) {
+        const toRemove = args[i];
+        while (arr.includes(toRemove)) {
+            idx =  arr.indexOf(toRemove)
+            arr.splice(idx, 1)
+        };
+    };
+    return arr
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (
+        typeof start != 'number' || 
+        typeof end != 'number' ||
+        !Number.isInteger(start) ||
+        !Number.isInteger(end) ||
+        start < 0 || 
+        end < 0) return "ERROR";
+    if (start > end) [start, end] = [end, start];
+    let fullArr = []
+    for (i=start; i<=end; i++) fullArr.push(i);
+    let result = fullArr.reduce((sum, num) => (sum += num), 0);
+    return result
 };
 
 // Do not edit below this line
